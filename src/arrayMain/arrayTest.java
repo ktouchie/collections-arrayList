2fe390e031f84276ec7f3630d97ae529207d8696
Karin Touchie <ktouchie@gmail.com>
2016-02-24 14:52:56 +0100

    Final commit

--- src/arrayMain/arrayTest.java
+++ src/arrayMain/arrayTest.java
@@ -1,7 +1,12 @@
 package arrayMain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.Enumeration;
 
 public class arrayTest {
 
@@ -20,7 +25,39 @@ public class arrayTest {
 		for(int i=0; i<al.size(); i++){
 			System.out.println("donnée à l'indice " + i + " = " + al.get(i));
 		}
-
+		
+		// déclare une collection
+		Set hs = new HashSet();
+		
+		// ajout d'élément dans la collection
+		hs.add(12);
+		hs.add("Une chaine de caratères");
+		hs.add('d');
+		hs.add('d');
+		
+		// parcours de la collection
+		Iterator it = hs.iterator();
+		while(it.hasNext()){
+			System.out.println(it.next());
+		}
+		
+		// déclare une collection
+		Hashtable ht = new Hashtable();
+		
+		//Map ht = new Hashtable();
+		
+		// ajout d'éléments dans la collection
+		ht.put(1, "primptemps");
+		ht.put(10, "ete");
+		ht.put(12, "automne");
+		ht.put(45, "hiver");
+		
+		// parcours de la collection
+		Enumeration e = ht.elements();
+		
+		while(e.hasMoreElements()){
+			System.out.println(e.nextElement());
+		}
 	}
 
 }
